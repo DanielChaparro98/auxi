5036a72c88bcefb1117967dddc5b6f0107d0fca4
Daniel Chaparro <dchaparrov1@ucentral.edu.co>
2024-09-13 10:13:18 -0500

    Ajuste servicios

--- support-microservice/src/main/kotlin/com/microservice/support/controller/PetitionController.kt
+++ support-microservice/src/main/kotlin/com/microservice/support/controller/PetitionController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/petition")
 @RequiredArgsConstructor
